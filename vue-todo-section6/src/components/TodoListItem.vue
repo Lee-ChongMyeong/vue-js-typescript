9cd8fc36d39d13f6e754c65c2c53f5444a42e331
이총명 <dlchdaud1327@gmail.com>
2021-12-24 22:44:54 +0900

    - vue-todo-section 5 - vue-todo-section 6 - vue-news-ts

---
+++ vue-todo-section6/src/components/TodoListItem.vue
@@ -0,0 +1,50 @@
+<template>
+    <li>
+        <span class ="item"
+         :class="todoItemClass"
+         @click="toggleItem"
+         > {{ todoItem.title }} </span>
+        <!-- <button @click="$emit('delete')">삭제</button> -->
+        <button @click="removeItem">삭제</button>
+        <!-- 버튼에다가 바로 실무에서 수용가능한 코드임, 하지만 테스트 하기 어려운 코드임.  -->
+    </li>
+</template>
+
+<script lang="ts">
+import Vue, { PropType} from 'vue'
+import { Todo } from '../App.vue';
+
+export default Vue.extend({
+    props: {
+        todoItem: Object as PropType<Todo>,
+        index: Number
+    },
+
+    computed: {
+        todoItemClass(): string | null {
+            return this.todoItem.done ? 'complete' : null
+        }
+    },
+
+    methods: {
+        toggleItem(){
+            this.$emit("toggle", this.todoItem, this.index);
+        },
+
+        removeItem(){
+            this.$emit("remove", this.index);
+        }
+    }
+})
+</script>
+
+<style scoped>
+.item{
+    cursor: pointer;
+}
+
+.complete{
+    text-decoration: line-through;
+}
+
+</style>
